592e1bd6b3ee8a110527ef9a1d0bb9d7aaf012eb
Alireza <alireza2000sajedi@gmail.com>
2024-08-15 18:17:51 +0330

    fix: response type

--- src/Facades/Responder.php
+++ src/Facades/Responder.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class Responder
  *
- * @method static \Illuminate\Http\Response respond(array $data = [])
- * @method static \Illuminate\Http\Response respondPaginate(array $data)
+ * @method static \Illuminate\Http\JsonResponse respond(array $data = [])
+ * @method static \Illuminate\Http\JsonResponse respondPaginate(array $data)
  * @method static self setData(array $data)
  * @method static self setMessage(string $message)
  * @method static self appendData(array $data)
@@ -17,16 +17,16 @@ use Illuminate\Support\Facades\Facade;
  * @method static self created(array $data = [])
  * @method static self updated(array $data = [])
  * @method static self deleted()
- * @method static \Illuminate\Http\Response ok(array $data = [])
+ * @method static \Illuminate\Http\JsonResponse ok(array $data = [])
  * @method static self setLinks(array $links)
  * @method static self setMeta(array $meta)
- * @method static \Illuminate\Http\Response badRequest()
- * @method static \Illuminate\Http\Response tooManyRequests()
- * @method static \Illuminate\Http\Response notFound()
- * @method static \Illuminate\Http\Response internalError(mixed $exception = null)
+ * @method static \Illuminate\Http\JsonResponse badRequest()
+ * @method static \Illuminate\Http\JsonResponse tooManyRequests()
+ * @method static \Illuminate\Http\JsonResponse notFound()
+ * @method static \Illuminate\Http\JsonResponse internalError(mixed $exception = null)
  * @method static void validationError(array $errors = [])
- * @method static \Illuminate\Http\Response unauthorized()
- * @method static \Illuminate\Http\Response unauthenticated()
+ * @method static \Illuminate\Http\JsonResponse unauthorized()
+ * @method static \Illuminate\Http\JsonResponse unauthenticated()
  */
 class Responder extends Facade
 {
