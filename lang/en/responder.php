9b3f41175480b50ae64b8d218fd746797bb91f3f
Alireza <alireza2000sajedi@gmail.com>
2024-08-15 16:54:28 +0330

    Add lang files

---
+++ lang/en/responder.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /**********         Statuses & Operations        ************/
+    "success"          => "Operation successful!",
+    "error"            => "Operation encountered an error!",
+    "validation_error" => "Validation failed!",
+    "internal_error"   => "Internal server error!",
+    "created"          => ":attribute has been created.",
+    "updated"          => ":attribute has been updated.",
+    "deleted"          => ":attribute has been deleted.",
+    "send"             => ":attribute has been sent.",
+    "before_posted"    => ":attribute has already been posted.",
+    "expire"           => ":attribute has expired.",
+    "not_valid"        => ":attribute is not valid.",
+    "used"             => ":attribute has already been used.",
+    "re_send"          => ":attribute has been resent.",
+    "unauthenticated"  => "User is not authenticated!",
+    "unauthorized"     => "User is not authorized to access this resource!",
+    "not_found"        => "Resource not found!",
+    'bad_request'      => 'Bad request!',
+    'to_many_request'  => 'Too many requests!',
+];
---
+++ lang/fa/responder.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /**********         Statuses & Operations        ************/
+    "success"          => "عملیات با موفقیت انجام شد!",
+    "error"            => "عملیات با خطا مواجه شد!",
+    "validation_error" => "خطا در اعتبارسنجی مقادیر ورودی!",
+    "internal_error"   => "خطای داخلی سرور!",
+    "created"          => ":attribute با موفقیت ایجاد شد.",
+    "updated"          => ":attribute با موفقیت به‌روز شد.",
+    "deleted"          => ":attribute با موفقیت حذف شد.",
+    "send"             => ":attribute با موفقیت ارسال شد.",
+    "before_posted"    => ":attribute قبلاً ارسال شده است.",
+    "expire"           => ":attribute منقضی شده است.",
+    "not_valid"        => ":attribute معتبر نمی‌باشد.",
+    "used"             => ":attribute قبلاً استفاده شده است.",
+    "re_send"          => ":attribute با موفقیت مجدداً ارسال شد.",
+    "unauthenticated"  => "شما احراز هویت نشده‌اید!",
+    "unauthorized"     => "شما مجوز دسترسی به این بخش را ندارید!",
+    "not_found"        => "صفحه مورد نظر یافت نشد!",
+    'bad_request'      => 'درخواست نادرست است!',
+    'to_many_request'  => 'تعداد درخواست‌ها بیش از حد مجاز است!',
+];
